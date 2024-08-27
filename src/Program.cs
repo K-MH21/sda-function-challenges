ad51f6d0718d27ed2dd82ca1f884b68031ab0b03
Kazero21 <Thinker7120@hotmail.com>
2024-08-27 13:47:08 +0300

    Finish Challenge 1

--- src/Program.cs
+++ src/Program.cs
@@ -6,8 +6,38 @@ namespace FunctionChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("start from here");
+            int challenge = 1; // Edit this to access the challenges
+            switch (challenge)
+            {
+                case 1:
+                    Console.WriteLine(StringNumberProcessor("Hello", 100, 200, "World"));
+                    Console.WriteLine(StringNumberProcessor("Apple", "Banana", 50, 150, "Cherry"));
+                    Console.WriteLine(StringNumberProcessor(10, 20, 30));
+                    Console.WriteLine(StringNumberProcessor(500, "Mixed", 1000, "Types", 300));
+                    Console.WriteLine(StringNumberProcessor("Single", 1));
+                    Console.WriteLine(StringNumberProcessor("Only", "Strings", "Here"));
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
 
+        private static string StringNumberProcessor(params object[] args)
+        {
+            string words = "";
+            double numbers = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] is string)
+                    words += " " + args[i];
+                else
+                    numbers += Convert.ToDouble(args[i]);
+            }
+            return $"{words};{numbers}";
         }
     }
 }
