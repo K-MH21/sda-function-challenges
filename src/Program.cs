1af8d4190165c112c8e7bbd6880ed508877ecc56
Kazero21 <Thinker7120@hotmail.com>
2024-08-27 14:24:56 +0300

    Finish Challenge 2

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 
 namespace FunctionChallenges
 {
@@ -6,7 +7,7 @@ namespace FunctionChallenges
     {
         static void Main(string[] args)
         {
-            int challenge = 1; // Edit this to access the challenges
+            int challenge = 2; // Edit this to access the challenges
             switch (challenge)
             {
                 case 1:
@@ -18,11 +19,39 @@ namespace FunctionChallenges
                     Console.WriteLine(StringNumberProcessor("Only", "Strings", "Here"));
                     break;
                 case 2:
+                    GuessingGame();
                     break;
                 case 3:
                     break;
                 case 4:
                     break;
+            }
+        }
+
+        private static void GuessingGame()
+        {
+            int number = new Random().Next(9) + 1; // 1 to 10
+            Console.WriteLine("Guess a number between 1 and 10");
+            while (true)
+            {
+                int guess = Convert.ToInt16(Console.ReadLine());
+                if (1 > guess | guess > 10)
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+                switch (guess.CompareTo(number))
+                {
+                    case -1:
+                        Console.WriteLine("Your guess is smaller; guess a larger number");
+                        break;
+                    case 0:
+                        Console.WriteLine("You guessed the number correctely!");
+                        return;
+                    case 1:
+                        Console.WriteLine("Your guess is larger; guess a smaller number");
+                        break;
+                }
             }
         }
 
